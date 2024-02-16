e224c137fe37225648ef3c6aaca1eb45d237217d
Ankith T V <tvankith@gmail.com>
2024-02-17 01:12:40 +0530

    chore: eslint integration

---
+++ components/styles.module.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly 'blocks': string
+  readonly 'header': string
+  readonly 'header--fixed': string
+  readonly 'header--right': string
+  readonly 'scroller': string
+  readonly 'wrapper': string
+}
+export = styles
