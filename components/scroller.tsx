a6bc842a723a662d19c561ac44ac0f1098763a8a
Ankith T V <tvankith@gmail.com>
2024-02-17 01:12:40 +0530

    chore: eslint integration

--- components/scroller.tsx
+++ components/scroller.tsx
@@ -1,84 +1,85 @@
-import React  from 'react'
-import { useLayoutEffect, useRef } from 'react'
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
+
+import React, { useLayoutEffect, useRef } from 'react'
 import styles from './styles.module.css'
 
-const Scroll = (props: { children: React.ReactNode; gap: string; className?: string }) => {
-  
-  const scrollerRef = useRef<HTMLDivElement>() 
+const Scroll = (props: { children: React.ReactNode, gap: string, className?: string }): React.ReactNode => {
+  const scrollerRef = useRef<HTMLDivElement>()
 
-  useLayoutEffect(()=> {
-    const scroller = scrollerRef.current as HTMLDivElement
-    if(scroller) {
-      scroller.style.setProperty("--block-gap", props.gap)
-      const allBlocks = scroller?.querySelectorAll(`.${styles["blocks"]}`) as NodeListOf<HTMLDivElement>
+  useLayoutEffect(() => {
+    const scroller = scrollerRef.current
+    if (scroller != null) {
+      scroller.style.setProperty('--block-gap', props.gap)
+      const allBlocks: NodeListOf<HTMLElement> = scroller?.querySelectorAll(`.${styles.blocks}`)
       Array.from(allBlocks).forEach((blocks) => {
-        const header = blocks.querySelector(`.${styles["header"]}`) as HTMLDivElement
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const header = blocks.querySelector(`.${styles.header}`)!
         const headerHeight = header.getBoundingClientRect().height
-        blocks.style.setProperty("--top-padding", headerHeight + "px")
+        blocks.style.setProperty('--top-padding', headerHeight + 'px')
       })
     }
-  },[props.gap])
+  }, [props.gap])
 
-  const onScroll = () => {
-    const scroller = scrollerRef.current as HTMLDivElement
-    const allBlocks = scroller?.querySelectorAll(`.${styles["blocks"]}`) as NodeListOf<HTMLDivElement>
+  const onScroll = (): void => {
+    const scroller = scrollerRef.current
+    if (scroller == null) return
+    const allBlocks = scroller.querySelectorAll(`.${styles.blocks}`) as NodeListOf<HTMLDivElement>
     let blockRightOffset = 0
     let blockLefttOffset = 0
     Array.from(allBlocks).forEach((blocks, index, array) => {
       const scrollOffset = scroller.scrollLeft
-      const gap = parseInt(props.gap.split("px")[0])
-      blockRightOffset+= index === array.length - 1 ? blocks.getBoundingClientRect().width : blocks.getBoundingClientRect().width + gap
-      blockLefttOffset+= index === 0 ? 0 : allBlocks[index - 1].getBoundingClientRect().width + gap
-      const header = blocks.querySelector(`.${styles["header"]}`) as HTMLDivElement
+      const gap = parseInt(props.gap.split('px')[0])
+      blockRightOffset += index === array.length - 1 ? blocks.getBoundingClientRect().width : blocks.getBoundingClientRect().width + gap
+      blockLefttOffset += index === 0 ? 0 : allBlocks[index - 1].getBoundingClientRect().width + gap
+      // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+      const header = blocks.querySelector(`.${styles.header}`) as HTMLDivElement
       const headerWidth = header.getBoundingClientRect().width
-      const blockWidth = index === array.length - 1 ? blocks.getBoundingClientRect().width: blocks.getBoundingClientRect().width + gap
+      const blockWidth = index === array.length - 1 ? blocks.getBoundingClientRect().width : blocks.getBoundingClientRect().width + gap
       const scrollerLeftOffset = scroller.getBoundingClientRect().left
 
       // Left aligned
-      if(scrollOffset <= blockLefttOffset) {
-        header.classList.remove(styles["header--right"])
-        header.classList.remove(styles["header--fixed"])
+      if (scrollOffset <= blockLefttOffset) {
+        header.classList.remove(styles['header--right'])
+        header.classList.remove(styles['header--fixed'])
         return
       }
       // Right aligned
-      if(blockRightOffset - scrollOffset -gap < headerWidth) {
-        header.style.setProperty("--left-offset", scrollOffset + blockWidth - headerWidth + "px")
-        header.classList.remove(styles["header--fixed"])
-        header.classList.add(styles["header--right"])
+      if (blockRightOffset - scrollOffset - gap < headerWidth) {
+        header.style.setProperty('--left-offset', scrollOffset + blockWidth - headerWidth + 'px')
+        header.classList.remove(styles['header--fixed'])
+        header.classList.add(styles['header--right'])
         return
       }
       // Fixed & Left aligned
-      header.classList.add(styles["header--fixed"])
-      header.classList.remove(styles["header--right"])
-      header.style.setProperty("--left-offset", scrollerLeftOffset + "px")
-      return
+      header.classList.add(styles['header--fixed'])
+      header.classList.remove(styles['header--right'])
+      header.style.setProperty('--left-offset', scrollerLeftOffset + 'px')
     })
   }
-  return <div onScroll={onScroll} className={`${styles["scroller"]} ${props.className ? props.className: ''}`} ref={scrollerRef as React.LegacyRef<HTMLDivElement>}>
+  return <div onScroll={onScroll} className={`${styles.scroller} ${props.className ?? ''}`} ref={scrollerRef as React.LegacyRef<HTMLDivElement>}>
     {props.children}
   </div>
 }
 
-
-const Group = (props: { children: React.ReactNode }) => {
+const Group = (props: { children: React.ReactNode }): React.ReactNode => {
   return (
-    <div className={styles["blocks"]}>
+    <div className={styles.blocks}>
       {props.children}
     </div>
   )
 }
 
-const Title = (props: { children: React.ReactNode }) => {
+const Title = (props: { children: React.ReactNode }): React.ReactNode => {
   return (
-    <div className={styles["header"]}>
+    <div className={styles.header}>
       {props.children}
     </div>
   )
 }
 
-const Item = (props: { children: React.ReactNode; className?: string }) => {
+const Item = (props: { children: React.ReactNode, className?: string }): React.ReactNode => {
   return (
-    <div className={`${styles["block"]} ${props.className}`}>
+    <div className={`${props.className}`}>
       {props.children}
     </div>
   )
@@ -90,4 +91,4 @@ Scroll.Title = Title
 
 Scroll.Item = Item
 
-export default Scroll;
+export default Scroll
--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,32 @@
 import { resolve } from 'node:path'
-import { terser } from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser'
 
 import * as packageJson from './package.json'
 import dts from 'vite-plugin-dts'
 
-const defineConfig = ((configEnv) => ({
-    plugins: [dts({
-      entryRoot: './components'
-    })],
-    css: {
-      modules: true
+const defineConfig = (configEnv) => ({
+  plugins: [dts({
+    entryRoot: './components'
+  })],
+  css: {
+    modules: true
+  },
+  build: {
+    minify: 'terser',
+    lib: {
+      entry: resolve('.', 'components/index.ts'),
+      name: 'ReactViteLibrary',
+      formats: ['es'],
+      fileName: (format) => 'index.js'
     },
-    build: {
-      minify: 'terser',
-      lib: {
-        entry: resolve('.', 'components/index.ts'),
-        name: 'ReactViteLibrary',
-        formats: ['es'],
-        fileName: (format) => `index.js`,
-      },
-      rollupOptions: {
-        external: [...Object.keys(packageJson.peerDependencies)],
-        plugins: [terser()]
-      },
-    },
-    optimizeDeps: {
-      include: ['react'],
-    },
-}))
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+      plugins: [terser()]
+    }
+  },
+  optimizeDeps: {
+    include: ['react']
+  }
+})
 
 export default defineConfig
